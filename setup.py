2a4cff35705fb90d7bd0e52e266e98d08ef5f680
qeternity <ch@se.gd>
2018-04-28 19:04:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_b2',
+    version='0.1',
+    packages=find_packages(),
+    license=open("LICENSE").read(),
+    long_description=open('README.md').read()
+)
